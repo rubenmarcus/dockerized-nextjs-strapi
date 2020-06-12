c9c7bfcc5b6fa2245848105cb5662011ac25128e
rubenmarcus <ruben@rubenmarcus.com>
2020-06-12 01:25:24 -0300

    CMS

---
+++ cms/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd22b9328-428c-4d3e-b3ca-bdcc123deb2e'
+};
--- cms/app/readme.md
+++ cms/app/readme.md
@@ -1,1 +1,3 @@
-aaa
+# Strapi application
+
+A quick description of your strapi application
